711c91cd099fc255a20bdad0fe12aee8e4097829
Angie-Santos <ajsag12@gmail.com>
2022-11-30 09:37:08 -0500

    Solución challenge 05

--- challenge05/challenge05.js
+++ challenge05/challenge05.js
@@ -1,30 +1,32 @@
 const users = 'https://codember.dev/mecenas.json';
 
 const challenge = (arrayUsers) => {
-  console.log('RONDA')
-  console.log('Array Users:', arrayUsers.length);
+
   const winners = [];
 
-  for (let i = 0; i < arrayUsers.length; i+=2){
+  for (let i = 0; i < arrayUsers.length; i += 2) {
     winners.push(arrayUsers[i])
   }
 
-  if (winners.length > 1 && arrayUsers.length % 2 !== 0) {
-      winners.shift()
+  if (arrayUsers.length > 1 && arrayUsers.length % 2 !== 0) {
+    winners.shift()
   }
 
-  if (winners.length === 1) {
-    console.log(winners, '---------\n')
+  if (arrayUsers.length === 1) {
     return winners[0]
   } else {
-    console.log(winners)
+
     return challenge(winners, '---------\n')
   }
 };
 
 fetch(users)
-.then((response) => response.json())
-.then((users) => {
-  console.log('INDEX', users.indexOf(challenge(users)));
-})
+  .then((response) => response.json())
+  .then((users) => {
+    const arrObj = users.map((user, index)=> {
+      const obj = {index, user}
+      return obj;
+    })
+    console.log('INDEX', challenge(arrObj));
+  })
 
